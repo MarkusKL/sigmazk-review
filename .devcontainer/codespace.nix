2d9e3f4526680075d712ae480dce5ce216a64ffa
Markus Krabbe Larsen <markus@krabbelarsen.dk>
2026-08-27 14:48:49 +0200

    update devcontainer conf

--- .devcontainer/codespace.nix
+++ .devcontainer/codespace.nix
@@ -1,7 +1,9 @@
 let
   args = { bundle = "9.1"; inNixShell = true; };
-  nixpkgs = import ./default.nix args;
+  nixpkgs = import ../default.nix args;
   pkgs = nixpkgs.pkgs;
+  # push cache:
+  # nix-build codespace.nix | cachix push markuskl-ssprove
 in pkgs.mkShell {
   packages = with pkgs; [
     coqPackages.coq
